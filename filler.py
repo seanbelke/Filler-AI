8050c82ee1152aa011abf635dc4199ae09e15ac1
Sean Belke <69130259+seanbelke@users.noreply.github.com>
2022-03-29 11:45:44 -0400

    Update filler.py

--- filler.py
+++ filler.py
@@ -2,6 +2,7 @@ from gamesearch import *
 from enum import Enum
 from copy import deepcopy
 
+
 class Color(Enum):
     RED = 1
     GREEN = 2
@@ -25,31 +26,26 @@ class Color(Enum):
             return 'PUR'
 
 
-
 class FillerBoard():
     def __init__(self, colors, player='A'):
         self.board = colors
         self.width = len(colors)  # not canonical indexing
         self.height = len(colors[0])
         self.player = player
-    
+
     def __repr__(self):
-        return '\n'.join([x.__repr__() for x in FillerBoard.unrotate_grid(self.board)])
-    
-    def unrotate_grid(grid):
-        rows = len(grid)
-        cols = len(grid[0])
-        return [[grid[r][c] for r in range(rows)] for c in range(cols -1, -1, -1)]
+        return '\n'.join([x.__repr__() for x in unrotate_grid(self.board)])
 
     def get_player_a_color(self):
         return self.board[0][0]
-    
+
     def get_player_b_color(self):
         return self.board[self.width - 1][self.height - 1]
-    
+
     def get_player_region(self, player):
-        color = self.board[0][0]
-        region = [(0, 0)]
+        if player == 'A':
+            color = self.board[0][0]
+            region = [(0, 0)]
         if player == 'B':
             color = self.board[self.width - 1][self.height - 1]
             region = [(self.width - 1, self.height - 1)]
@@ -74,15 +70,14 @@ class FillerBoard():
         (x, y) = loc
         if x not in range(self.width) or y not in range(self.height):
             return False
-        return self.board[x][y] == color      
+        return self.board[x][y] == color
 
 
 class Filler(Game):
-    def __init__(self, initial, max_search_depth = 4):
+    def __init__(self, initial, max_search_depth=4):
         super().__init__(initial, ['A', 'B'])
         self.search_depth = max_search_depth
 
-    
     def to_move(self, s):
         return s.player
 
@@ -90,22 +85,22 @@ class Filler(Game):
         actions = set([Color.RED, Color.GREEN, Color.BLUE, Color.BLACK, Color.YELLOW, Color.PURPLE])
         actions.discard(s.get_player_a_color())
         actions.discard(s.get_player_b_color())
-        new_actions = set([])
-        for a in actions:
-            if any([a in row for row in s.board]):
-                new_actions.add(a)
-        return new_actions
-    
+        # new_actions = set([])
+        # for a in actions:
+        #     if any([a in row for row in s.board]):
+        #         new_actions.add(a)
+        return actions # was return new_actions
+
     def result(self, s, a):
-        newboard = [[col for col in row] for row in s.board]
+        new_board = [[col for col in row] for row in s.board]
         region = s.get_player_region(s.player)
         for (x, y) in region:
-            newboard[x][y] = a
+            new_board[x][y] = a
         new_player = 'A'
         if s.player == 'A':
             new_player = 'B'
-        return FillerBoard(newboard, new_player)
-    
+        return FillerBoard(new_board, new_player)
+
     def is_terminal(self, s):
         player_a_score = len(s.get_player_region('A'))
         if player_a_score > (s.width * s.height) // 2:
@@ -115,19 +110,24 @@ class Filler(Game):
             return True
         elif (player_a_score + player_b_score) == (s.width * s.height):
             return True
+
         return False
-    
+
     def is_cutoff(self, s, depth):
-        if depth >= self.search_depth:
-            return True
-        else:
-            return self.is_terminal(s)
-    
+        return depth >= self.search_depth or self.is_terminal(s)
+
     def utility(self, s, p):
-        notp = 'A'
         if p == 'A':
             notp = 'B'
+        else:
+            notp = 'A'
         return len(s.get_player_region(p)) - len(s.get_player_region(notp))
+
+
+def unrotate_grid(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+    return [[grid[r][c] for r in range(rows)] for c in range(cols - 1, -1, -1)]
 
 def rotate_grid(grid):
     rows = len(grid)
@@ -136,8 +136,7 @@ def rotate_grid(grid):
     return [[newgrid[r][c] for c in range(rows - 1, -1, -1)] for r in range(cols)]
 
 
-
-def play_perfect_game(game, initial=None):
+def play_game(game, initial=None):
     s = initial or game.initial
     while not game.is_terminal(s):
         a_score = len(s.get_player_region('A'))
@@ -148,12 +147,10 @@ def play_perfect_game(game, initial=None):
         print('To Move: ' + s.player)
         print('Actions: ' + str(game.actions(s)))
         print()
-        # move = alpha_beta_minimax(game, s)
-        move = None
         if game.to_move(s) == 'A':
-            move = alpha_beta_minimax(game, s)
+            move = alpha_beta_minimax(game, s) # AI plays for player A
         else:
-            move = Color(int(input("move: ")))
+            move = Color(int(input("move: "))) # Player B must input a move
         s = game.result(s, move)
     a_score = len(s.get_player_region('A'))
     b_score = len(s.get_player_region('B'))
@@ -162,12 +159,11 @@ def play_perfect_game(game, initial=None):
     print(s)
     print()
 
-    a_util = game.utility(s, 'A')
-    b_util = game.utility(s, 'B')
-    winner = 'A'
-    if b_score > a_score:
+    if a_score > b_score:
+        winner = 'A'
+    elif b_score > a_score:
         winner = 'B'
-    elif b_score == a_score:
+    else:
         winner = 'Tie'
     print("winner: " + winner)
 
@@ -186,28 +182,16 @@ initial_grid = [
 ]
 
 initial_grid = [
-    [GR, BU, RD, YL, PU, BK, YL, BU],
-    [YL, GR, BK, BU, BK, YL, PU, RD],
-    [PU, BK, RD, PU, BU, GR, RD, PU],
-    [BK, PU, BU, YL, GR, RD, PU, BK],
-    [BU, RD, BK, RD, PU, BU, RD, GR],
-    [PU, BK, GR, YL, GR, PU, GR, BK],
-    [GR, RD, YL, GR, BU, BK, RD, GR]
+    [PU, BK, GR, BU, GR, RD, YL, GR],
+    [BU, GR, PU, BK, PU, YL, PU, BU],
+    [YL, RD, GR, RD, YL, GR, RD, PU],
+    [BU, GR, BK, YL, BK, PU, GR, BU],
+    [GR, RD, BU, RD, PU, BK, PU, RD],
+    [YL, PU, GR, BU, RD, GR, YL, BK],
+    [BK, BU, PU, GR, BU, PU, BK, BU]
 ]
 
-initial_board = FillerBoard(rotate_grid(initial_grid), 'B')
+initial_board = FillerBoard(rotate_grid(initial_grid), 'A')
 
 game = Filler(initial_board)
-play_perfect_game(Filler(initial_board, 8))
-
-
-        
-
-
-
-        
-        
-
-        
-
-
+play_game(Filler(initial_board, 8))
